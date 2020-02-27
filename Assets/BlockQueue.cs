e6a8730f05572a4e75b52eaf704f0e9fd0925a73
Luke Mirman <OrangeJuicePanda1@gmail.com>
2020-02-26 19:52:00 -0500

    Work in progress code

---
+++ Assets/BlockQueue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockQueue : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject[] levelBlocks;
+	private Queue<GameObject> blocks = new Queue<GameObject>();
+
+	public int Count => blocks.Count;
+
+	private void Awake()
+	{
+		blocks = new Queue<GameObject>(levelBlocks);
+	}
+
+	public GameObject GetNextBlock()
+	{
+		return blocks.Dequeue();
+	}
+}
